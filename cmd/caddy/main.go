61265fcaa963c80ee1ce2c73e6e28e2dcbe3e284
YI <uuuuuu@protonmail.com>
2025-04-26 17:36:52 +0800

    remove some unused packages

--- cmd/caddy/main.go
+++ cmd/caddy/main.go
@@ -4,17 +4,14 @@ import (
 	caddycmd "github.com/caddyserver/caddy/v2/cmd"
 
 	// plug in Caddy modules here
-	_ "github.com/abiosoft/caddy-json-schema"
 	_ "github.com/caddy-dns/cloudflare"
 	_ "github.com/caddyserver/replace-response"
-
-	_ "github.com/contrun/mycaddy/pkg/postgres-storage"
 	_ "github.com/greenpau/caddy-security"
-	_ "github.com/imgk/caddy-trojan"
 	_ "github.com/mholt/caddy-l4"
-	_ "github.com/mholt/dhall-adapter"
 	_ "github.com/mohammed90/caddy-storage-loader"
 	_ "github.com/techknowlogick/certmagic-s3"
+
+	_ "github.com/contrun/mycaddy/pkg/postgres-storage"
 )
 
 func main() {
